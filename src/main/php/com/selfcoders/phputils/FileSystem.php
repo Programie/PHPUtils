f5e1b4fe262b83b5d40f55848db3aa32992a5d8d
Michael Wieland <github@selfcoders.com>
2016-09-14 16:05:11 +0200

    Allow to specify alternative units in FileSystem::formatFileSize()

--- src/main/php/com/selfcoders/phputils/FileSystem.php
+++ src/main/php/com/selfcoders/phputils/FileSystem.php
@@ -3,8 +3,15 @@ namespace com\selfcoders\phputils;
 
 class FileSystem
 {
-    public static function formatFileSize($value)
+    /**
+     * Format the given size in bytes to a human readable file size.
+     *
+     * @param int $value The size in bytes
+     * @param array $units An array of units
+     * @return string The formatted file size (e.g. "100 MB")
+     */
+    public static function formatFileSize($value, $units = array("B", "KB", "MB", "GB", "TB"))
     {
-        return Format::getHumanReadableUnit($value, 1024, $units = array("B", "KB", "MB", "GB", "TB"));
+        return Format::getHumanReadableUnit($value, 1024, $units);
     }
 }
